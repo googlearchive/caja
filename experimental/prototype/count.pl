17b9b6ea34c7dc11e3074a62a0b0396dec76394a
Ben Laurie <ben@links.org>
2008-12-11 15:43:06 +0000

    Update stats and improve calculation methodology.
    git-svn-id: https://google-caja.googlecode.com/svn/trunk@3122 360991c0-9f3b-0410-ba87-8b362a00edf4

--- experimental/prototype/count.pl
+++ experimental/prototype/count.pl
@@ -6,22 +6,39 @@ use Carp;
 open(F,'status') || croak "status: $!";
 
 my($tt, $at, $ft, $et);
-while(my $line=<F>) {
+my($ptt, $pat, $pft, $pet);
+
+while(my $line = <F>) {
     chomp $line;
 
-    next if $line !~ /(.+): (\d+) tests, (\d+) assertions, (\d+) failures, (\d+) errors/;
+    if ($line =~ /(.+): (\d+) tests, (\d+) assertions, (\d+) failures, (\d+) errors/) {
+	my($m, $t, $a, $f, $e) = ($1, $2, $3, $4, $5);
 
-    my($m, $t, $a, $f, $e) = ($1, $2, $3, $4, $5);
+	print "$m: $t tests, $a assertions, $f failures, $e errors\n";
 
-    print "$m: $t tests, $a assertions, $f failures, $e errors\n";
+	$tt += $t;
+	$at += $a;
+	$ft += $f;
+	$et += $e;
 
-    $tt+=$t;
-    $at+=$a;
-    $ft+=$f;
-    $et+=$e;
+	$line = <F>;
+	chomp $line;
+	print "$line\n";
+	croak if $line !~ /\((\d+) tests, (\d+) assertions, (\d+) failures, (\d+) errors\)/;
+
+	($t, $a, $f, $e) = ($1, $2, $3, $4);
+
+	$ptt += $t;
+	$pat += $a;
+	$pft += $f;
+	$pet += $e;
+    } 
 }
 
-print "\nTotal: $tt tests, $at assertions, $ft failures, $et errors\n\n";
+print "\nTotal: $tt tests, $at assertions, $ft failures, $et errors";
+print "\n      ($ptt tests, $pat assertions, $pft failures, $pet errors)\n\n";
 
-printf "Assertion failures: %.2f%%\n", $ft*100./$at;
-printf "Errors            : %.2f%%\n", $et*100./$tt;
+croak if $tt != $ptt;
+
+printf "Assertion failures: %.2f%% (%.2f%%)\n", $ft*100./$pat, $pft*100./$pat;
+printf "Errors            : %.2f%% (%.2f%%)\n", $et*100./$tt, $pet*100./$tt;
