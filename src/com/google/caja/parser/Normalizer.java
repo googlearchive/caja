931174900bffb296ad7d6ac2877594bbbf4e797a
Mike Samuel <mikesamuel@gmail.com>
2008-12-11 04:01:03 +0000

    Issue 10444  http://codereview.appspot.com/10444 Change ParserBase to compile under JDK1.5.
    ParserBase has a compile time dependency on java.text.Normalizer,
    though some tricks are done to avoid a runtime dependency.
    
    Tobie needs to compile on an old version of MacOS X, so this
    uses nasty reflection to get rid of the compile time dependency.
    
    It also adds comments around the fallback code that David-Sarah
    dug up, and expands to all of ISO-8859-1.
    
    R=ihab.awad@gmail.com
    
    
    git-svn-id: https://google-caja.googlecode.com/svn/trunk@3115 360991c0-9f3b-0410-ba87-8b362a00edf4

---
+++ src/com/google/caja/parser/Normalizer.java
@@ -0,0 +1,97 @@
+// Copyright (C) 2008 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.caja.parser;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+/**
+ * Provides a similar interface to {@link java.text.Normalizer}, but will
+ * compile and run on JDK1.5 in a stricter mode.
+ *
+ * @author mikesamuel@gmail.com
+ */
+final class Normalizer {
+  private static final Method IS_NORMALIZED;
+  private static final Object NORMAL_FORM_C;
+
+  static {
+    Method isNormalized = null;
+    Object normalFormC = null;
+    try {
+      Class<?> normalizer = Class.forName("java.text.Normalizer");
+      Class<?> normalizerForm = Class.forName("java.text.Normalizer$Form");
+
+      isNormalized = normalizer.getMethod(
+          "isNormalized", CharSequence.class, normalizerForm);
+      normalFormC = normalizerForm.getField("NFC").get(null);
+    } catch (ClassNotFoundException ex) {
+      // Don't use the normalizer.
+      // Use null from above.
+    } catch (IllegalAccessException ex) {
+      throw new RuntimeException(ex);
+    } catch (NoSuchFieldException ex) {
+      throw new RuntimeException(ex);
+    } catch (NoSuchMethodException ex) {
+      throw new RuntimeException(ex);
+    }
+
+    IS_NORMALIZED = isNormalized;
+    NORMAL_FORM_C = normalFormC;
+  }
+
+  /**
+   * A conservative heuristic as to whether s is normalized according to Unicode
+   * Normal Form C.  It is heuristic, because Caja needs to run with versions
+   * of the Java standard libraries that do not include normalization.
+   * @return false if s is not normalized.
+   */
+  public static boolean isNormalized(CharSequence s) {
+    if (IS_NORMALIZED != null) {
+      try {
+        return ((Boolean) IS_NORMALIZED.invoke(null, s, NORMAL_FORM_C))
+            .booleanValue();
+      } catch (IllegalAccessException ex) {
+        throw new RuntimeException(ex);
+      } catch (InvocationTargetException ex) {
+        Throwable th = ex.getTargetException();
+        if (th instanceof RuntimeException) {
+          throw (RuntimeException) th;
+        } else {
+          throw (Error) th;
+        }
+      }
+    }
+
+    // From http://unicode.org/reports/tr15/#D6
+    // Legacy character sets are classified into three categories
+    // based on their normalization behavior with accepted
+    // transcoders.
+    // 1. Prenormalized. Any string in the character set is already in
+    //    Normalization Form X.
+    //    For example, ISO 8859-1 is prenormalized in NFC.
+    // ...
+    for (int i = s.length(); --i >= 0;) {
+      char ch = s.charAt(i);
+      // Codepoints in [32, 126] U [160, 255] are identical in both Unicode and
+      // ISO 8859-1.
+      // Codepoints in [0, 31] and [127, 159] are not part of ISO 8859-1.  They
+      // are control characters in Unicode, and disallowed in identifiers so
+      // will never reach here.
+      if (ch >= 256) { return false; }
+    }
+    return true;
+  }
+}
