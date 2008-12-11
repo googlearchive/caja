f0c924262af26a610b544aaa6c2909f5c7bc8cda
Jasvir Nagra <jasvir@gmail.com>
2008-12-11 06:11:06 +0000

    Issue 10251  http://codereview.appspot.com/10251 Userfriendlier testbed
    Get rid of no outcome exception if there is no output eg. cajoling pure html
    Alias alert to console.log
    
    R=mikesamuel@gmail.com
    
    
    git-svn-id: https://google-caja.googlecode.com/svn/trunk@3119 360991c0-9f3b-0410-ba87-8b362a00edf4

--- src/com/google/caja/opensocial/applet/testbed.js
+++ src/com/google/caja/opensocial/applet/testbed.js
@@ -408,15 +408,16 @@ var getImports = (function () {
                            [___.frozenFunc(newModule)]);
       } finally {
         var outcome = superHandler.getLastOutcome();
+        var outvalue = superHandler.getLastValue();
         var type = document.createElement('span');
         type.className = 'type';
-        type.appendChild(document.createTextNode(typeString(outcome[1])));
+        type.appendChild(document.createTextNode(typeString(outvalue)));
 
         var entry = document.createElement('div');
         entry.className = 'result';
         entry.appendChild(type);
-        entry.appendChild(document.createTextNode(repr(outcome[1])));
-        if (!outcome[0]) {
+        entry.appendChild(document.createTextNode(repr(outvalue)));
+        if (!(outcome && outcome[0])) {
           // TODO(erights): color something red
         }
         document.getElementById('eval-results' + uiSuffix)
@@ -471,6 +472,8 @@ var getImports = (function () {
               ? gadgetPublicApis[moduleName]
               : void 0;
         });
+    /** Provide an alert but one that is less obnoxious */
+    testImports.alert = cajita.log;
 
     testImports.newModuleHandler___ = 
       makeNewModuleHandler(testImports, uiSuffix);
