af58cd56601c34540a9f06bd0ca7161827eb78d0
Jasvir Nagra <jasvir@gmail.com>
2010-02-02 05:06:08 +0000

    Allow more than one gadget per page on the playground http://codereview.appspot.com/198052
    Change enable to take an optional id parameter to specify the div to use
    
    R=ihab.awad@gmail.com
    
    
    git-svn-id: https://google-caja.googlecode.com/svn/trunk@3966 360991c0-9f3b-0410-ba87-8b362a00edf4

--- src/com/google/caja/demos/playground/taming.js
+++ src/com/google/caja/demos/playground/taming.js
@@ -52,151 +52,8 @@ var caja___ = (function () {
     ___.grantFunc(imports.outers.swfobject, 'embedSWF');
   }
 
-  // Advanced flash taming that does allows a flash object attentuated access to the page
-  function tameBridgedFlash(imports) {
-    imports.outers.gadgets = {};
-    imports.outers.gadgets.flash = {};
-    imports.outers.gadgets.flash.embedFlash = (function () {
-      var cleanse = (function () {
-        // Gets a fresh Array and Object constructor that 
-        // doesn't have the caja properties on it.  This is 
-        // important for passing objects across the boundary 
-        // to flash code.
-        var ifr = document.createElement("iframe");
-        ifr.width = 1; ifr.height = 1; ifr.border = 0;
-        document.body.appendChild(ifr);
-        var A = ifr.contentWindow.Array;
-        var O = ifr.contentWindow.Object;
-        document.body.removeChild(ifr);
-      
-        var c = function(obj) {
-          var t = typeof obj, i;
-          if (t === 'number' || t === 'boolean' || t === 'string') { 
-              return obj; 
-          }
-          if (t === 'object') {
-            var o;
-            if (obj instanceof Array) { o = new A; }
-            else if (obj instanceof Object) { o = new O; }
-            for (i in obj) {
-              if (/__$/.test(i)) { continue; }
-              o[i] = c(obj[i]);
-            }
-            return o;
-          }
-          return (void 0);
-        };
-        return c;
-      })();
-  
-      return ___.frozenFunc(function tamedEmbedFlash(
-             swfUrl, 
-             swfContainer,
-             swfVersion, 
-             opt_params) {
-        // Check that swfContainer is a wrapped node
-        if (typeof swfContainer === "string") {
-          // This assumes that there's only one gadget in the frame.
-          var $v = ___.getNewModuleHandler().getImports().$v;
-          swfContainer = $v.cm(
-              $v.ro("document"), 
-              "getElementById", 
-              [swfContainer]);
-        } else if (typeof swfContainer !== "object" || !swfContainer.node___) {
-          return false;
-        }
-  
-        // Generate a random number for use as the channel name
-        // for communication between the bridge and the contained
-        // flash object.
-        // TODO: Use true randomness.
-        var channel = "_flash" + ("" + Math.random()).substring(2);
-  
-        // Strip out allowNetworking and allowScriptAccess, 
-        //   as well as any caja-specific properties.
-        var new_params = {};
-        for (i in opt_params) {
-          if (i.match(/___$/)) { continue; }
-          var ilc = i.toLowerCase();
-          if (ilc === "allownetworking" || ilc === "allowscriptaccess") {
-            continue;
-          }
-          var topi = typeof opt_params[i];
-          if (topi !== "string" && topi !== "number") { continue; }
-          new_params[i] = opt_params[i];
-        }
-        new_params.allowNetworking = "never";
-        new_params.allowScriptAccess = "none";
-        if (!new_params.flashVars) { new_params.flashVars = ""; }
-        new_params.flashVars += "&channel=" + channel;
-  
-        // Load the flash.
-        gadgets.flash.embedFlash(swfUrl, swfContainer.node___, 10, new_params);
-  
-        if (bridge___.channels) {
-          // If the bridge hasn't loaded, queue up the channel names
-          // for later registration
-          bridge___.channels.push(channel);
-        } else {
-          // Otherwise, register the channel immediately.
-          bridge___.registerChannel(channel);
-        }
-  
-        // Return the ability to talk to the boxed swf.
-        return ___.primFreeze({
-          callSWF: (function (channel) { 
-            return ___.func(function (methodName, argv) {
-                return bridge___.callSWF(
-                    "" + channel, 
-                    "" + methodName, 
-                    cleanse(argv));
-              });
-          })(channel)
-        });
-      });
-    })());
-  
-    var d = document.createElement('div');
-    d.appendChild(document.createTextNode("bridge"));
-    document.body.appendChild(d);
-    
-    gadgets.flash.embedFlash(
-        "Bridge.swf", 
-        d,
-        10,
-        {
-          allowNetworking: "always",
-          allowScriptAccess: "all",
-          width: 0,
-          height: 0,
-          flashvars: "logging=true"
-        });
-    bridge___ = d.childNodes[0];
-    bridge___.channels = [];
-    
-    callJS = function (functionName, argv) {
-      // This assumes that there's a single gadget in the frame.
-      var $v = ___.getNewModuleHandler().getImports().$v;
-      return $v.cf($v.ro(functionName), [argv]);
-    };
-        
-    onFlashBridgeReady = function () {
-      var len = bridge___.channels.length;
-      for(var i = 0; i < len; ++i) {
-        bridge___.registerChannel(bridge___.channels[i]);
-      }
-      delete bridge___.channels;
-      var outers = ___.getNewModuleHandler().getImports().$v.getOuters();
-      if (outers.onFlashBridgeReady) {
-        callJS("onFlashBridgeReady");
-      }
-    };
-  
-  }
-
   function tameFlash(imports) {
-    tameSimpleFlash();
-    tameBridgedFlash();
+    tameSimpleFlash(imports);
   }
 
 
@@ -209,15 +66,15 @@ var caja___ = (function () {
           cajita.log(msg);
         } else {
           if (remainingAlerts > 0) {
-    	    remainingAlerts--;
-    	    alert(msg);
-    	  } else {
-    	    if (confirm("Redirect remaining alerts to console?")) {
-    	      useConsole = true;
-    	    } else {
-    	      remainingAlerts = 10;
-    	    }
-    	  }
+            remainingAlerts--;
+            alert(msg);
+          } else {
+            if (confirm("Redirect remaining alerts to console?")) {
+              useConsole = true;
+            } else {
+              remainingAlerts = 10;
+            }
+          }
         }
       };
       return tameAlert;
@@ -225,7 +82,8 @@ var caja___ = (function () {
     ___.grantFunc(imports.outers, 'alert');
   }
   
-  function enable() {
+  function enable(divElId) {
+    divElId = divElId || 'cajoled-output';
     var imports = ___.copy(___.sharedImports);
     imports.outers = imports;
     var uriCallback = {
@@ -237,12 +95,12 @@ var caja___ = (function () {
               + '&mimeType=' + encodeURIComponent(mimeType));
       }
     };
-
-    var gadgetRoot = document.getElementById('cajoled-output');
+    var gadgetRoot = document.getElementById(divElId);
     gadgetRoot.className = cajaDomSuffix;
     
     imports.htmlEmitter___ = new HtmlEmitter(gadgetRoot);
-    attachDocumentStub('-' + cajaDomSuffix, uriCallback, imports, gadgetRoot);
+    attachDocumentStub('-' + divElId + '-' + cajaDomSuffix, uriCallback,
+        imports, gadgetRoot);
     imports.$v = valijaMaker.CALL___(imports.outers);
     ___.getNewModuleHandler().setImports(imports);
     
