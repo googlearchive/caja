dc96bec8352d775e3a31fed9018702a88e1ae47e
Mike Samuel <mikesamuel@gmail.com>
2010-03-03 01:35:59 +0000

    Upgrade appengine to 1.3.1 http://codereview.appspot.com/223104
    R=jasvir@gmail.com
    
    
    git-svn-id: https://google-caja.googlecode.com/svn/trunk@4003 360991c0-9f3b-0410-ba87-8b362a00edf4

--- third_party/java/appengine/bin/dev_appserver.sh
+++ third_party/java/appengine/bin/dev_appserver.sh
@@ -4,6 +4,6 @@
 SDK_BIN=`dirname $0 | sed -e "s#^\\([^/]\\)#${PWD}/\\1#"` # sed makes absolute
 SDK_LIB=$SDK_BIN/../lib
 SDK_CONFIG=$SDK_BIN/../config/sdk
-java -ea -cp $SDK_LIB/appengine-tools-api.jar \
+java -ea -cp "$SDK_LIB/appengine-tools-api.jar" \
   com.google.appengine.tools.KickStart \
   com.google.appengine.tools.development.DevAppServerMain $*
