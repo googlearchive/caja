49ef8968c713b3819bc2a42bd6fd27e91ea25545
Kevin Reid <kpreid@switchb.org>
2010-07-21 17:39:36 +0000

    Corkboard demo: improve handling of cajoling errors.
    * If the cajoler service reports an error about input (HTTP 400 per issue 1249/r4185) then display the error log. (Could be improved; see issue 1250.)
    * Whether the error is expected or unexpected, highlight it in the display of postings.
    * Generate a dummy Caja module rather than no JS at all if the cajoler fails, so the browser console does not show cryptic errors for that.
    
    
    git-svn-id: https://google-caja.googlecode.com/svn/trunk@4193 360991c0-9f3b-0410-ba87-8b362a00edf4

--- src/com/google/caja/demos/corkboard/cajole.py
+++ src/com/google/caja/demos/corkboard/cajole.py
@@ -17,31 +17,68 @@ from django.utils import simplejson as json
 import urllib2
 import hashlib
 import logging
+import re
 
+# config
 cajaServer = "http://caja.appspot.com/"
 memcacheNamespace = "cajoled"
 
+# constants
+cajoleRequestURL = cajaServer + "cajole?input-mime-type=text/html" \
+                                    + "&output-mime-type=application/json"
+dummyModule = "___.loadModule({'instantiate': function () {}})"
+
+def cajolingErrorModule(e):
+  """Given a HTTP 400 error (as presented by urllib2), return a cajoling-result
+  dict showing the error."""
+  # mmm, kludge. TODO(kpreid): After
+  # <http://code.google.com/p/google-caja/issues/detail?id=1250>.
+  # is fixed, make use of it here.
+  errorHtml = e.read()
+  errorMatch = re.search(r'(?s)(<pre>.*</pre>)', errorHtml)
+  if errorMatch:
+    errorHtml = errorMatch.group(1)
+  errorHtml = "<div><strong>Cajoling error</strong></div>" + errorHtml
+  return {"html": errorHtml, "js": dummyModule, "error": True}
+
 def cajole(html):
   """ Given HTML, return a dict of its cajoled form with keys 'html' and 'js'.
+  
+  If the cajoling fails, the dict will also have the key 'error' with a true
+  value and the HTML will describe the error.
   """
   if html == "":
     # workaround for http://code.google.com/p/google-caja/issues/detail?id=1248
-    return {"html":"","js":""}
+    return {"html": "", "js": dummyModule}
   key = hashlib.sha1(html).digest()
   value = memcache.get(key, namespace=memcacheNamespace)
   if value is None:
+    logging.debug("Cache miss (HTML sha1 " + key + "); invoking cajoler.")
     try:
-      result = urllib2.urlopen(urllib2.Request(
-        (cajaServer + "cajole"
-         + "?input-mime-type=text/html"
-         + "&output-mime-type=application/json"),
-        html,
-        {"Content-Type": "text/html"}))
-      value = json.load(result)
+      try:
+        result = urllib2.urlopen(urllib2.Request(
+          cajoleRequestURL,
+          html,
+          {
+            "Content-Type": "text/html",
+            "Accept": "application/json",
+          }))
+        value = json.load(result)
+      except urllib2.HTTPError, e:
+        if e.code == 400:
+          # cajoler's input error
+          value = cajolingErrorModule(e)
+        else:
+          raise
     except Exception, e:
-      # TODO(kpreid): catch HTTP 400 errors (the cajoler input was rejected)
-      # and permit the user to view or reproduce them.
       logging.exception("Error in invoking cajoler.")
-      return {"html": "<strong>(Caja error)</strong>", "js": ""}
+      # don't put in cache, might be a transient error
+      # TODO(kpreid): when not debugging, DO put in cache with a shorter timeout
+      # for high-load handling
+      return {
+        "html": "<strong>(Unexpected Caja error)</strong>", 
+        "js": dummyModule,
+        "error": True
+      }
     memcache.add(key, value, namespace=memcacheNamespace)
   return value
