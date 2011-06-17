35483c20d80710baf71aeb77038520021df2b240
Ihab Awad <ihab.awad@gmail.com>
2011-06-17 04:28:26 +0000

    Update 'Numbers' demo to new API and cleaner impl.
    git-svn-id: https://google-caja.googlecode.com/svn/trunk@4508 360991c0-9f3b-0410-ba87-8b362a00edf4

--- src/com/google/caja/demos/numbers/main.js
+++ src/com/google/caja/demos/numbers/main.js
@@ -12,171 +12,59 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-var api = (function() {
-
-  var data = (function() {
-    var makeRandomData = function(n) {
-      var result = [];
-      for (var i = 0; i < n; i++) {
-	result[i] = Math.random();
-      }
-      return result;
-    };
-
-    var makeSeries = function(n, step) {
-      var result = [];
-      for (var i = 0; i < n; i++) {
-	result[i] = i * step;
-      }
-      return result;
-    };
-
-    var makeColumn = function(name, units, data) {
-      var min = 0, max = 0;
-      if (data.length) {
-	min = max = data[0];
-	for (var i = 0; i < data.length; i++) {
-	  min = Math.min(min, data[i]);
-	  max = Math.max(max, data[i]);
-	}
-      }
-      return {
-        header: {
-          name: name,
-          units: units,
-          min: min,
-          max: max
-        },
-        values: data
-      };
-    };
-
-    var numRows = 100;
-
-    var columns = [
-      makeColumn('Time',        'sec',    makeSeries(numRows, .25)),
-      makeColumn('Density',     'kg/m^3', makeRandomData(numRows)),
-      makeColumn('Temperature', 'degK',   makeRandomData(numRows)),
-      makeColumn('Pressure',    'N/m^2',  makeRandomData(numRows))
-    ];
-
-    var getNumRows = function() { return numRows; };
-
-    var getNumCols = function() { return columns.length; };
-
-    var getColHeader = function(j) {
-      if (j < 0 || j >= columns.length) { return undefined; }
-      return columns[j].header;
-    };
-
-    var get = function(i, j) {
-      if (i < 0 || i >= numRows) { return undefined; }
-      if (j < 0 || j >= columns.length) { return undefined; }
-      return columns[j].values[i];
-    };
-
-    return {
-      getNumRows: getNumRows,
-      getNumCols: getNumCols,
-      getColHeader: getColHeader,
-      get: get,
-    };
-  })();
-
-  var selection = undefined;
-  var selectionListeners = [];
-
-  // The selection getter function must ensure that it does not expose
-  // mutable internal state to the guest. In this case, it returns a
-  // defensive copy of the 'selection' array. Our taming copies arrays
-  // across the taming boundary anyway, but we do not recommend that Caja
-  // users rely on this for the moment.
-  var getSelection = function() {
-    return selection ? selection.slice(0) : undefined;
-  };
-
-  // The selection setter function is called by guest code and must defend
-  // itself against bad input
-  var setSelection = function(sel) {
-    if (!sel) { return; }
-    var boundedNumber = function(x, bound) {
-      x = Number(x);
-      if (x < 0) {
-        return 0; 
-      } else if (x >= bound) {
-        return bound - 1;
-      } else {
-        return x;
-      }
-    };
-    selection = [
-      boundedNumber(sel[0], data.nRows),
-      boundedNumber(sel[1], data.nCols)
-    ];
-    fireSelectionChanged();
-  };
-
-  // When the host calls a function provided by guest code, it must provide
-  // a safe 'this' value to avoid exposing its internal state to the guest
-  var fireSelectionChanged = function() {
-    for (var i = 0; i < selectionListeners.length; i++) {
-      selectionListeners[i].call({});
-    }
-  };
-
-  // Host code must ensure that selection listeners provided by guest code
-  // will not cause host code to throw unexpectedly, since that may cause the
-  // host to violate its invariants
-  var addSelectionListener = function(l) {
-    if (typeof l === 'function') { selectionListeners.push(l); }
-  };
-
-  return {
-    getSelection: getSelection,
-    setSelection: setSelection,
-    addSelectionListener: addSelectionListener,
-    data: data
-  };
-})();
-
 var log = function(s) {
-  document.getElementById('log').innerHTML += s + '\n';
+  console.log(s);
 };
 
-var addGadgetDiv = function() {
+var addGadgetDiv = function(name) {
   var td = document.createElement('td');
   td.style.verticalAlign = 'top';
+  var title = document.createElement('h3');
+  title.innerHTML = name;
   var outerDiv = document.createElement('div');
   document.getElementById('gadgets').appendChild(td);
+  td.appendChild(title);
   td.appendChild(outerDiv);
   return outerDiv;
 };
 
 var defaultUriPolicy = {
-  rewrite: function(uri, mimeType) { return undefined; }
+  rewrite: function(uri, mimeType) {
+    return undefined;
+  }
+};
+
+var tame = function(frameGroup, api) {
+  frameGroup.markFunction(api.getSelection);
+  frameGroup.markFunction(api.setSelection);
+  frameGroup.markFunction(api.addSelectionListener);
+  frameGroup.markFunction(api.data.getNumRows);
+  frameGroup.markFunction(api.data.getNumCols);
+  frameGroup.markFunction(api.data.getColHeader);
+  frameGroup.markFunction(api.data.get);
+  return frameGroup.tame(api);
+};
+
+var loadGadget = function(frameGroup, tameApi, name, url) {
+  var extraOuters = {
+    api: tameApi,
+    log: frameGroup.tame(frameGroup.markFunction(function(s) {
+      log(name + ' (' + url + ') : ' + String(s));
+    }))
+  };
+
+  frameGroup.makeES5Frame(addGadgetDiv(name), defaultUriPolicy, function (frame) {
+    frame.url(url).run(extraOuters, function (result) { 
+      log('Gadget ' + name + ' (' + url + ') loaded');
+    });
+  });
 };
 
 caja.configure({
-  cajaServer: 'http://caja.appspot.com/',
+  cajaServer: 'http://localhost:8080/',
   debug: true
 }, function(frameGroup) {
-
-  var tameApi = frameGroup.tame(api);
-  
-  function loadGadget(url) {
-    frameGroup.makeES5Frame(addGadgetDiv(), defaultUriPolicy, function (frame) {
-      var extraOuters = {
-	api: tameApi,
-	log: frameGroup.tame(function(s) {
-          log(url + ': ' + String(s));
-	})
-      };
-      frame.run(url, extraOuters, function (result) { 
-	log('Gadget ' + url + ' loaded');
-      });
-    });
-  }
-
-  loadGadget('./table.html');
-  loadGadget('./graph.html');
+  var tameApi = tame(frameGroup, api /* from api.js */);
+  loadGadget(frameGroup, tameApi, 'Prudhoe Bay 2002', './table.html');
+  loadGadget(frameGroup, tameApi, 'Prudhoe Bay 2002', './graph.html');
 });
