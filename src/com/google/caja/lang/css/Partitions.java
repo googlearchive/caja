328ca29c8a123ddc19d8d5b53fb657247bbf1b66
Mike Samuel <mikesamuel@gmail.com>
2011-11-11 23:28:32 +0000

    Supporting class for new CSS Schema->JavaScript converter http://codereview.appspot.com/5372053
    This is a supporting class for the rewrite of CssPropertyPatterns which
    produces css-defs.js.
    
    It doesn't have anything directly related to CSS, but is instead a bunch of
    set manipulation code.
    
    This will be used to compute a partition of a set of CSS literals so that
    we can support a larger subset of the CSS schema without blowing up the
    amount of code we ship to the client.
    
    Right now, css-defs.js use a simplistic definition of color because <color>
    appears in many CSS property definitions, and is a very large production
    including keywords for each entry in the X11 color table.
    
    By computing a partition of keywords when compiling css-defs.js we can
    support the full color definition because the partition computer realizes
    that all the color keywords appear together and so it can create one set
    of color name keywords regardless of how many CSS properties use those.
    
    R=metaweta@gmail.com
    
    
    git-svn-id: https://google-caja.googlecode.com/svn/trunk@4670 360991c0-9f3b-0410-ba87-8b362a00edf4

---
+++ src/com/google/caja/lang/css/Partitions.java
@@ -0,0 +1,360 @@
+// Copyright (C) 2011 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.caja.lang.css;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.BitSet;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import com.google.caja.util.Bag;
+import com.google.caja.util.Lists;
+import com.google.caja.util.Maps;
+
+class Partitions {
+  static class Partition<T> {
+    /**
+     * For each input set S, a set of indices I into partition such that
+     * <ol>
+     * <li>S' is the minimal set s.t. i is in S' when universe[i] is in S.
+     * <li>S' = union( partition[i] for i in I ).
+     * </ul>
+     */
+    int[][] unions;
+    /**
+     * The union of the input sets, sorted if comparable or if a comparator
+     * was supplied.
+     */
+    T[] universe;
+    /**
+     * A partition of the set (0..universe.length).
+     * Each element is a sorted-unique array of indices into universe.
+     * Each element is disjoint w.r.t. every other element.
+     */
+    int[][] partition;
+  }
+
+  /** A monotonically increasing slice of an {@code int[]}. */
+  private static class ISlice {
+    /**
+     * Elements between 0 (inclusive) and count (exclusive) are strictly
+     * monotonically increasing.
+     */
+    int[] els;
+    /** Count of usable elements in els. */
+    int count;
+
+    /** Modify {@code this} to remove any elements in r. */
+    boolean subtract(ISlice r) {
+      int i = 0, j = 0, k = 0;
+      while (i < count && j < r.count) {
+        int a = els[i], b = r.els[j];
+        if (a < b) {
+          els[k++] = els[i++];
+        } else if (a == b) {
+          ++i;
+          ++j;
+        } else {
+          ++j;
+        }
+      }
+      if (i != count) {
+        int n = count - i;
+        System.arraycopy(els, i, els, k, n);
+        k += n;
+      }
+      if (count != k) {
+        count = k;
+        return true;  // The row changed.
+      }
+      return false;
+    }
+
+    /** Modify {@code this} to be the intersection of this and r. */
+    void preserve(ISlice r) {
+      int i = 0, j = 0, k = 0;
+      while (i < count && j < r.count) {
+        int a = els[i], b = r.els[j];
+        if (a < b) {
+          ++i;
+        } else if (a == b) {
+          els[k++] = a;
+          ++i;
+          ++j;
+        } else {
+          ++j;
+        }
+      }
+      count = k;
+    }
+
+    boolean contains(int el) {
+     return Arrays.binarySearch(els, 0, count, el) >= 0;
+    }
+
+    @Override
+    public ISlice clone() {
+      ISlice clone = new ISlice();
+      clone.els = toArray();
+      clone.count = count;
+      return clone;
+    }
+
+    int[] toArray() {
+      return Arrays.copyOfRange(els, 0, count);
+    }
+
+    @Override public String toString() {
+      return Arrays.toString(toArray());
+    }
+  }
+
+  /**
+   * Computes a partition of union(sets) such that each set can be
+   * efficiently represented as a union of a subset of that partition.
+   *
+   * <p>
+   * This may be very inefficient if sets contains a large number of empty
+   * elements.
+   */
+  static <T> Partition<T> partition(
+      Iterable<? extends Iterable<T>> sets, Class<T> type,
+      Comparator<?> cmp) {
+    // Compute the unique elements of sets.
+    T[] universe;
+    int nSets = 0;
+    Map<T, Integer> elToIndex = Maps.newHashMap();
+    {
+      Bag<T> bag = Bag.newHashBag();
+      for (Iterable<T> set : sets) {
+        for (T el : set) {
+          bag.incr(el);
+        }
+        ++nSets;
+      }
+      Set<T> uniq = bag.nonZeroCounts();
+      // Have to box to fit in a HashBag.
+      universe = allocateReferenceTypeArray(type, uniq.size());
+      {
+        int i = 0;
+        for (T el : uniq) { universe[i++] = el; }
+      }
+
+      tryToSort(universe, cmp);
+      for (int i = 0; i < universe.length; ++i) {
+        elToIndex.put(universe[i], i);
+      }
+    }
+
+    // Compute a table from sets that includes a row per input set.
+    // The row is a sorted array slice of indices into universe.
+    // We will destructively modify rows when we create a partition element by
+    // removing all elements from each row that are in the partition so we also
+    // maintain a count of non zero rows and columns.
+    ISlice nonZeroCols = new ISlice();
+    nonZeroCols.els = new int[nonZeroCols.count = universe.length];
+    for (int i = 0; i < nonZeroCols.count; ++i) { nonZeroCols.els[i] = i; }
+
+    ISlice[] matrix = new ISlice[nSets];
+    int matrixSum = 0;
+    {
+      int i = 0;
+      BitSet rowBits = new BitSet(universe.length);
+      for (Iterable<T> set : sets) {
+        rowBits.clear();
+        for (T el : set) {
+          int index = elToIndex.get(el);
+          if (!rowBits.get(index)) {
+            rowBits.set(index);
+            ++matrixSum;
+          }
+        }
+        ISlice row = new ISlice();
+        row.count = rowBits.cardinality();
+        row.els = new int[row.count];
+        for (int b = -1, j = 0; (b = rowBits.nextSetBit(b+1)) >= 0;) {
+          row.els[j++] = b;
+        }
+        Arrays.sort(row.els);
+        matrix[i++] = row;
+      }
+    }
+
+    ISlice nonZeroRows = new ISlice();
+    nonZeroRows.els = new int[nSets];
+    for (int i = 0; i < matrix.length; ++i) {
+      if (matrix[i].count != 0) {
+        nonZeroRows.els[nonZeroRows.count++] = i;
+      }
+    }
+
+
+    // An ordered list of disjoint sets.
+    List<ISlice> partition = Lists.newArrayList();
+    // For each input sets, the indices of elements in partition that must be
+    // unioned to produce the corresponding set T as described in the javadoc.
+    ISlice[] unionIndices = new ISlice[nSets];
+    for (int i = 0; i < nSets; ++i) {
+      unionIndices[i] = new ISlice();
+      unionIndices[i].els = new int[universe.length];
+    }
+
+    while (nonZeroRows.count != 0) {
+      ISlice bestPartitionElement = null;
+      for (int i = 0; i < nonZeroCols.count; ++i) {
+        int el = nonZeroCols.els[i];
+        ISlice candidate = null;
+        // Compute the intersection of all rows containing el.
+        for (int j = 0; j < nonZeroRows.count; ++j) {
+          ISlice r = matrix[nonZeroRows.els[j]];
+          if (r.contains(el)) {
+            if (candidate == null) {
+              candidate = r.clone();
+            } else {
+              candidate.preserve(r);
+            }
+          }
+        }
+        // Subtract out anything that is in a set that does not contain el.
+        assert candidate != null;
+        for (int j = 0; j < nonZeroRows.count; ++j) {
+          ISlice r = matrix[nonZeroRows.els[j]];
+          if (!r.contains(el)) {
+            candidate.subtract(r);
+          }
+        }
+        // Now we know the maximal set of elements that always co-occur with el.
+        if (bestPartitionElement == null
+            || bestPartitionElement.count < candidate.count) {
+          bestPartitionElement = candidate;
+        }
+      }
+
+      // Add best to the partition and update the matrix by subtracting the
+      // columns in best, and any rows that are subsets of best.
+      assert bestPartitionElement != null;
+      int partitionIndex = partition.size();
+      partition.add(bestPartitionElement);
+
+      nonZeroCols.subtract(bestPartitionElement);
+      int k = 0;
+      for (int j = 0; j < nonZeroRows.count; ++j) {
+        int rowIndex = nonZeroRows.els[j];
+        ISlice r = matrix[rowIndex];
+        boolean changed = r.subtract(bestPartitionElement);
+        if (changed) {
+          unionIndices[rowIndex]
+              .els[unionIndices[rowIndex].count++] = partitionIndex;
+        }
+        if (r.count != 0) {
+          // Keep it in nonZeroRows.
+          nonZeroRows.els[k++] = rowIndex;
+        } else {
+          assert changed;
+        }
+      }
+      nonZeroRows.count = k;
+
+      if (bestPartitionElement.count == 1) {
+        // All candidates are equally bad, handle as long tail below.
+        break;
+      }
+    }
+
+    // Handle the long tail by creating a single element partition for each
+    // non-zero row.
+    {
+      int[] elementToPartition = new int[universe.length];
+      for (int i = 0; i < nonZeroCols.count; ++i) {
+        int el = nonZeroCols.els[i];
+        ISlice singleton = new ISlice();
+        singleton.count = 1;
+        singleton.els = new int[] { el };
+        elementToPartition[el] = partition.size();
+        partition.add(singleton);
+      }
+      nonZeroCols.count = 0;
+
+      for (int j = 0; j < nonZeroRows.count; ++j) {
+        int rowIndex = nonZeroRows.els[j];
+        ISlice r = matrix[rowIndex];
+        ISlice union = unionIndices[rowIndex];
+        for (int i = 0; i < r.count; ++i) {
+          int el = r.els[i];
+          union.els[union.count++] = elementToPartition[el];
+        }
+        r.count = 0;
+      }
+      nonZeroRows.count = 0;
+    }
+
+    // Construct the output.
+    Partition<T> result = new Partition<T>();
+    result.universe = universe;
+    result.unions = new int[unionIndices.length][];
+    for (int i = 0; i < unionIndices.length; ++i) {
+      result.unions[i] = unionIndices[i].toArray();
+    }
+    result.partition = new int[partition.size()][];
+    for (int i = 0; i < result.partition.length; ++i) {
+      result.partition[i] = partition.get(i).toArray();
+    }
+
+    return result;
+  }
+
+  @SuppressWarnings("unchecked")
+  private static <T> T[] allocateReferenceTypeArray(Class<T> type, int n) {
+    // If a primitive type was passed in, this is un-type safe, but that should
+    // not introduce type-unsafety since <T> in partition cannot bind to a
+    // primitive type unless some-other unchecked cast was unsafe.
+    if (type.isPrimitive()) { throw new AssertionError(type.getName()); }
+    // This is type-safe when type is not-primitive.
+    return (T[]) Array.newInstance(type, n);
+  }
+
+  @SuppressWarnings("unchecked")
+  private static <T> void tryToSort(T[] arr, Comparator<?> cmp) {
+    if (cmp == null) {
+      // Type safe.
+      final Class<T> tClazz = (Class<T>) arr.getClass().getComponentType();
+      if (!Comparable.class.isAssignableFrom(tClazz)) { return; }
+      // Don't use the natural ordering since that can't handle null.
+      cmp = new Comparator<Comparable<?>>() {
+        public int compare(Comparable<?> a, Comparable<?> b) {
+          T at = tClazz.cast(a), bt = tClazz.cast(b);
+          if (at == null) {
+            return bt == null ? 0 : -1;
+          } else if (bt == null) {
+            return 1;
+          }
+          // This is not technically type-safe, as at could implement
+          // Comparable<SomeDisjointType>.
+          // If at does obey the contract for Comparable it will, at worst,
+          // result in a ClassCastException.
+          return ((Comparable<? super T>) at).compareTo(bt);
+        }
+      };
+    }
+    // This is type-unsafe since a client can pass in a comparator of the
+    // wrong type.  This should result in a ClassCastException at worst which
+    // aborts partitioning.
+    Arrays.sort(arr, (Comparator<? super T>) cmp);
+  }
+
+}
---
+++ tests/com/google/caja/lang/css/PartitionsTest.java
@@ -0,0 +1,120 @@
+// Copyright (C) 2011 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.caja.lang.css;
+
+import java.util.Arrays;
+import java.util.BitSet;
+import java.util.List;
+
+import com.google.caja.util.CajaTestCase;
+import com.google.caja.util.Lists;
+import com.google.caja.util.MoreAsserts;
+import com.google.caja.util.Sets;
+import com.google.common.collect.ImmutableList;
+
+public class PartitionsTest extends CajaTestCase {
+  public final void testPartitions() {
+    List<String> a = ImmutableList.of("foo", "bar", "baz", "boo");
+    List<String> b = ImmutableList.of("baz", "boo", "foo", "far", "bar");
+    List<String> c = ImmutableList.of("boo", "boo", "far");
+    List<String> d = ImmutableList.of("unicorn", "yeti", "loch ness");
+    List<String> e = ImmutableList.of();
+    List<String> f = ImmutableList.of("rainbow");
+    List<String> g = ImmutableList.of("rainbow");
+    List<String> h = Lists.newArrayList((String) null, "yeti", "unicorn");
+
+    List<List<String>> sets = ImmutableList.of(a, b, c, d, e, f, g, h);
+
+    Partitions.Partition<String> p = Partitions.partition(
+        sets, String.class, null);
+
+    assertPartitionOf(p, sets);
+
+    assertPartition(
+        p,
+        "bar;baz;foo",
+        "unicorn;yeti",
+        "null",
+        "boo",
+        "far",
+        "loch ness",
+        "rainbow");
+  }
+
+  private <T> void assertPartitionOf(
+      Partitions.Partition<T> p, List<List<T>> sets) {
+    assertEquals(p.unions.length, sets.size());
+
+    // Make sure partition elements are disjoint and union to universe.
+    BitSet universe = new BitSet(p.universe.length);
+    universe.set(0, p.universe.length);
+    BitSet union = new BitSet(p.universe.length);
+    for (int[] partitionElement : p.partition) {
+      assertSortedAndUnique(partitionElement);
+      for (int elIndex : partitionElement) {
+        if (union.get(elIndex)) {
+          fail("partition elements not disjoint "
+              + Arrays.toString(p.partition));
+        }
+        union.set(elIndex);
+      }
+    }
+    assertEquals(universe, union);
+
+    // Make sure unioning reproduces the input sets.
+    for (int i = 0; i < sets.size(); ++i) {
+      List<T> set = sets.get(i);
+      List<T> actual = Lists.newArrayList();
+      for (int partitionIndex : p.unions[i]) {
+        for (int elIndex : p.partition[partitionIndex]) {
+          actual.add(p.universe[elIndex]);
+        }
+      }
+      assertEquals(
+          "" + set,
+          Sets.newLinkedHashSet(set),
+          Sets.newLinkedHashSet(actual));
+    }
+  }
+
+  private <T> void assertPartition(
+      Partitions.Partition<?> p, String... golden) {
+    String[] actual = new String[p.partition.length];
+    int i = 0;
+    for (int[] partition : p.partition) {
+      StringBuilder sb = new StringBuilder();
+      boolean first = true;
+      for (int elIndex : partition) {
+        if (first) {
+          first = false;
+        } else {
+          sb.append(';');
+        }
+        sb.append(p.universe[elIndex]);
+      }
+      actual[i++] = sb.toString();
+    }
+    MoreAsserts.assertListsEqual(Arrays.asList(golden), Arrays.asList(actual));
+  }
+
+  private void assertSortedAndUnique(int[] arr) {
+    if (arr.length == 0) { return; }
+    for (int i = 1; i < arr.length; ++i) {
+      if (arr[i - 1] >= arr[i]) {
+        fail("Not sorted and unique " + arr[i - 1] + " >= " + arr[i]);
+      }
+    }
+  }
+}
