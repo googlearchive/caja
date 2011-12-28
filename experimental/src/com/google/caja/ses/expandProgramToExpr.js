6c79e1fec1caa3b942e99e19de472067ec8da223
Mark Miller <erights@gmail.com>
2011-12-28 06:41:41 +0000

    More explanation
    git-svn-id: https://google-caja.googlecode.com/svn/trunk@4718 360991c0-9f3b-0410-ba87-8b362a00edf4

--- experimental/src/com/google/caja/ses/expandProgramToExpr.js
+++ experimental/src/com/google/caja/ses/expandProgramToExpr.js
@@ -13,37 +13,88 @@
 // limitations under the License.
 
 /**
- * @fileoverview Serves as pseudo-code until we can run it.
+ * @fileoverview Pseudo-code explaining the source-SES to target-SES
+ * translation.
  *
- * <p>See the "Translation" section of
- * http://code.google.com/p/google-caja/wiki/SES for the requirements
- * assumed by this code.
+ * <p>See <a href="http://code.google.com/p/google-caja/wiki/SES"
+ * >http://code.google.com/p/google-caja/wiki/SES</a> for an
+ * explanation of the differences between source-SES and target-SES.
+ *
+ * <p>Serves as pseudo-code until we can run it. This section explains
+ * what more is needed to turn this into running code.
+ *
+ * We express our translations using JavaScript <a href=
+ * "http://wiki.ecmascript.org/doku.php?id=harmony:quasis"
+ * >quasiliterals</a> and a hypothetical JavaScript quasi-parser named
+ * "js". We additionally assume that the expression syntax {a, b} is
+ * shorthand not for <pre>
+ * {a: a, b: b}
+ * </pre> as currently proposed, but rather for
+ * <pre>
+ * {
+ *   get a() { return a; },
+ *   set a(n) { a = n; },
+ *   get b() { return b; },
+ *   set b(n) { b = n; }
+ * }</pre> as proposed at the end of the <a href=
+ * "http://wiki.ecmascript.org/doku.php?id=strawman:object_initialiser_shorthand#discussion"
+ * >object initializer discussion</a> section.
+ *
+ * Together, this means that (quasiParser`stuff${{varName}}stuff`) can
+ * create an object that can be used as a pattern, placing the values
+ * extracted from the specimen into varName, which must have already
+ * been declared. As a further elaboration, we assume predicate
+ * pattern functions that wrap such a literal
+ * (quasiParser`stuff${isThing({varName})}stuff`) and report whether
+ * the proposed assignment meets it criteria. If not, that match
+ * fails.
+ *
+ * Separately from the rules below, we also prohibit identifiers
+ * ending in ___ (triple underbar) in the input, so that we may use
+ * them in the output without fear of capture.
+ *
+ * The JavaScript quasiParser accepts one additional element besides
+ * JavaScript syntax and $ holes. In addition, when an "*", "+", or
+ * "?" appears immediately to the right of a $ hole with no
+ * intervening space, then it is taken as a quantifier on the binding
+ * of the $ hole to consecutive AST elements.
+ *
+ * Besides the "js" quasi-parser, the following code depends on these
+ * helper functions.
+ * isStatementOrDecl
+ * isExpr
+ * isGlobal          - a global variable name?
+ * isFree            - a free variable name?
+ * isGlobalOrFree
+ * mustNotShadow     - verify name doesn't shadow name in this function
+ * getFirstTokenName - printed form of first token
+ * getChildren       - of an ast node
+ * makeAstLike       - like arg, with alternate children
+ *
+ * The expanded code assumes bindings for
+ * declareGlobal___
+ * defineGlobal___
+ * cleanErr___
+ *
+ * Additional names reserved for the output that should be prohibited
+ * on input
+ * global___
+ * e___
+ *
+ * <p>The input ast to all of the expand* functions below comes after
+ * parsing and scope analysis of a complete Program production, so
+ * that questions like isGlobal have an answer. If all the $-hole
+ * inputs to a call to the js quasi-parser are asts, then js
+ * quasi-parser produces pure ast fragments without scope analysis as
+ * its output. If and of the $-hole inputs are one property objects or
+ * their predicate wrappers, as explained above, then that call
+ * produces an object with a ".test(ast)" method which matches against
+ * the ast and, if true, assigns the extracted fragments to these
+ * singleton properties.
  *
  * @author Mark S. Miller
  * @provides expandProgramToExpr
  */
-
-// Besides the "js" quasi-parser, the following code depends on these
-// helper functions.
-// isStatementOrDecl
-// isExpr
-// isGlobal          - a global variable name?
-// isFree            - a free variable name?
-// isGlobalOrFree
-// mustNotShadow     - verify name doesn't shadow name in this function
-// getFirstTokenName - printed form of first token
-// getChildren       - of an ast node
-// makeAstLike       - like arg, with alternate children
-//
-// The expanded code assumes bindings for
-// declareGlobal___
-// defineGlobal___
-// cleanErr___
-//
-// Additional names reserved for the output that should be prohibited
-// on input
-// global___
-// e___
 
 var expandProgramToExpr;
 
@@ -51,9 +102,6 @@ var expandProgramToExpr;
    "use strict";
 
    /**
-    * The input ast to all of these comes after parsing and scope
-    * analysis of a complete Program production, so that questions
-    * like isGlobal have an answer. The js quasi-parser
     */
    expandProgramToExpr = function expandProgramToExpr(ast) {
      if (!isProgram.test(ast)) { return null; }
