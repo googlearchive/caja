381b99ca9990c6c4801844c88a586d4505a7665e
Kevin Reid <kpreid@switchb.org>
2013-01-14 22:10:59 +0000

    Add makeArrayLike.canBeFullyLive to SES whitelist; link explicit.html from test-index. https://codereview.appspot.com/7101053
    r5212 introduced the boolean-valued cajaVM.makeArrayLike.canBeFullyLive,
    but without adding it to the SES whitelist, thus causing spurious SES
    failures. Now added.
    
    Added a link to the SES reporter src/com/google/caja/ses/explicit.html
    from the manual tests section of test-index.html.
    
    R=erights@gmail.com
    
    
    git-svn-id: https://google-caja.googlecode.com/svn/trunk@5215 360991c0-9f3b-0410-ba87-8b362a00edf4

--- src/com/google/caja/ses/whitelist.js
+++ src/com/google/caja/ses/whitelist.js
@@ -141,7 +141,9 @@ var ses;
       stamp: t,
       makeSealerUnsealerPair: t,
 
-      makeArrayLike: {}
+      makeArrayLike: {
+        canBeFullyLive: t
+      }
     },
     WeakMap: {       // ES-Harmony proposal as currently implemented by FF6.0a1
       prototype: {
