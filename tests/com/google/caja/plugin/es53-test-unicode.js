c3fee298682da6d629668298f7a4178ea1f80358
Felix Lee <felix8a@gmail.com>
2013-03-01 23:16:31 +0000

    squelch various firefox errors https://codereview.appspot.com/7226058
    running browser tests on firefox generates a number of spurious
    error messages in the console.
    
    - there are a lot of messages about missing charset.  I didn't
    see an easy way to make Jetty respond with the right charset,
    so I just put <meta charset="utf-8"> in all the relevant files.
    
    - es53-test-domado-dom-guest has a test that compares a uri
    against 'localhost', which doesn't work with remote browsers.
    
    - es53-test-unicode has problems in how it checks whether its
    tests pass or not.
    
    - cajajs-bare-test will append 'passed' to a script node,
    causing a spurious eval error
    
    R=kpreid2
    
    
    git-svn-id: https://google-caja.googlecode.com/svn/trunk@5312 360991c0-9f3b-0410-ba87-8b362a00edf4

--- tests/com/google/caja/plugin/es53-test-unicode.js
+++ tests/com/google/caja/plugin/es53-test-unicode.js
@@ -26,45 +26,50 @@
   });
   
   function basicPassingTest(name, code, result) {
-    var api = { result : undefined };
-    caja.load(undefined, uriPolicy, function (frame) {
+    var ipc = { result: 'untouched' };
+    caja.load(undefined, uriPolicy, jsunitCallback(function (frame) {
       frame.code('http://test.fake/fake.url', 'text/javascript', code)
-        .api({ api: frame.tame(api) })
-        .run(function (_) {
-          assertEquals(result, api.result);
+        .api({ ipc: frame.tame(ipc) })
+        .run(jsunitCallback(function (_) {
+          assertEquals(result, ipc.result);
           jsunitPass(name);
-        });
-    });
+        }));
+    }));
   }
 
   function basicFailingTest(name, code) {
-    var api = {};
-    caja.load(createDiv(), uriPolicy, function (frame) {
+    var ipc = { result: 'untouched' };
+    var gotError = false;
+    caja.load(createDiv(), uriPolicy, jsunitCallback(function (frame) {
       // Uses html rather than js since the only way to catch
       // top-level errors thrown by guest code is by providing an
-      // onerror available only 
-      api.onerror =
-        frame.tame(frame.markFunction(function() { jsunitPass(name); }));
+      // onerror handler
+      var onerror =
+        frame.tame(frame.markFunction(function() {
+          gotError = true;
+        }));
       frame.code('http://test.fake/fake.url', 'text/html',
         '<script>' + code + '</script>')
-        .api(api)
-        .run(function (_) {
-          fail();
-        });
-    });
+        .api({ ipc: frame.tame(ipc), onerror: onerror })
+        .run(jsunitCallback(function (_) {
+          assertEquals('untouched', ipc.result);
+          assertTrue(gotError);
+          jsunitPass(name);
+        }));
+    }));
   }
 
   registerTest('testUnicode1', function () { basicPassingTest('testUnicode1', 
     // Only ascii
-    'api.result = 42;', 42); 
+    'ipc.result = 42;', 42); 
   });
   registerTest('testUnicode2', function () { basicPassingTest('testUnicode2', 
     // Identifier with encoded character
-    'var \\u0061 = 43; api.result = a;', 43);
+    'var \\u0061 = 43; ipc.result = a;', 43);
   });
   registerTest('testUnicode3', function () { basicPassingTest('testUnicode3',
     // Reserved keyword as identifier with encoded character
-    'var a = {}; a.wit\\u0068 = 44; api.result = a.wit\\u0068;', 44);
+    'var a = {}; a.wit\\u0068 = 44; ipc.result = a.wit\\u0068;', 44);
   });
 
   // TODO(jasvir): Re-enable this test in ES5 mode when unicode parsing upstream
@@ -75,38 +80,38 @@
       // Regex containing encoded characters (from jquery)
       ''
       + 'var ID = /#((?:[\\w\\u00c0-\\uFFFF\\-]|\\\\.)+)/;'
-      + 'api.result = "#\\u00610b\\u0200{}".match(ID)[1];'
+      + 'ipc.result = "#\\u00610b\\u0200{}".match(ID)[1];'
       , "a0b\u0200");
     });
   }
   registerTest('testUnicode5', function () { basicPassingTest('testUnicode5',
     // Identifier with unencoded unicode character
-    'var \u0100 = 46; api.result = \u0100;', 46);
+    'var \u0100 = 46; ipc.result = \u0100;', 46);
   });
   registerTest('testUnicode6', function () { basicPassingTest('testUnicode6',
     // Identifier with unencoded unicode character
-    'var \u00A0w = 47; api.result = w;', 47);
+    'var \u00A0w = 47; ipc.result = w;', 47);
   });
 
   // Encoded spaces are not allowed in identifiers
   registerTest('testUnicode7',
     function testUnicode7() {
       basicFailingTest('testUnicode7',
-        'var a\\u2009 = 46; api.result = a\\u2009;');
-    
+        'var a\\u2009z = 48; ipc.result = a\\u2009z;');
+
   });
 
   // Unencoded spaces are not allowed in identifiers
   registerTest('testUnicode8',
     function testUnicode8() {
       basicFailingTest('testUnicode8',
-        'var a\u2009 = 46; api.result = a\u2009;');
+        'var a\u2009z = 49; ipc.result = a\u2009z;');
   });
 
   // Issue1637 Parse breaks on unicode escapes if they consist of more than 4
   // hex characters 
   registerTest('testUnicode9', function () { basicPassingTest('testUnicode9',
-    'api.result = "\u003c123"', "\u003c123");
+    'ipc.result = "\u003c123"', "\u003c123");
   });
 
   readyToTest();
