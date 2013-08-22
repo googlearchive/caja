43eaddd72ad24c4d48047c9ddfe1c9f142174a1e
Felix Lee <felix8a@gmail.com>
2013-08-22 19:01:51 +0000

    increase es5 scan timeout to 60s https://codereview.appspot.com/13171044
    chrome 30 and the r5569 changes now take more than 20s to do
    an es5 scan. it's not a lot more than 20s, but I'm increasing the
    timeout to 60s to give us some headroom.
    
    R=kpreid2
    
    
    
    git-svn-id: https://google-caja.googlecode.com/svn/trunk@5572 360991c0-9f3b-0410-ba87-8b362a00edf4

--- tests/com/google/caja/plugin/MainBrowserTest.java
+++ tests/com/google/caja/plugin/MainBrowserTest.java
@@ -36,7 +36,7 @@ public class MainBrowserTest extends CatalogTestCase {
     if (entry.getLabel().startsWith("guest-scan-es53-")) {
       return 160000;    // msec
     } else if (entry.getLabel().startsWith("guest-scan-es5-")) {
-      return 20000;     // msec
+      return 60000;     // msec
     } else {
       return super.waitForCompletionTimeout();
     }
