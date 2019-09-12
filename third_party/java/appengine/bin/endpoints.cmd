3602d97f82bf9a41b29af7ba585ebb63dbd5ea63
Kevin Reid <kpreid@google.com>
2019-09-12 08:10:15 -0700

    Update App Engine SDK to 1.9.74.

--- third_party/java/appengine/bin/endpoints.cmd
+++ third_party/java/appengine/bin/endpoints.cmd
@@ -5,7 +5,8 @@ rem Tool for Google Cloud Endpoints.
 
 setlocal
 
-set SDK_LIB=%~dp0..\lib
+set SDK_BIN=%~dp0..
+set SDK_LIB=%SDK_BIN%\lib
 set JAR_FILE1=%SDK_LIB%\opt\tools\appengine-local-endpoints\v1\appengine-local-endpoints.jar
 set JAR_FILE2=%SDK_LIB%\opt\user\appengine-endpoints\v1\appengine-endpoints.jar
 
@@ -19,7 +20,7 @@ if NOT EXIST "%JAR_FILE2%" (
   exit /B 1
 )
 
-set CLASSPATH="%JAR_FILE1%";"%JAR_FILE2%";"%SDK_LIB%\shared\servlet-api.jar";"%SDK_LIB%\appengine-tools-api.jar";"%SDK_LIB%\opt\user\datanucleus\v1\jdo2-api-2.3-eb.jar";"%SDK_LIB%\user\*"
+set CLASSPATH="%JAR_FILE1%";"%JAR_FILE2%";"%SDK_BIN%\jetty94\jetty-home\lib\servlet-api-3.1.jar";"%SDK_LIB%\appengine-tools-api.jar";"%SDK_LIB%\opt\user\datanucleus\v1\jdo2-api-2.3-eb.jar";"%SDK_LIB%\user\*"
 
 java -cp %CLASSPATH% com.google.api.server.spi.tools.EndpointsTool %*
 
--- third_party/java/appengine/bin/endpoints.sh
+++ third_party/java/appengine/bin/endpoints.sh
@@ -3,25 +3,35 @@
 #
 # Tool for Google Cloud Endpoints.
 
-[ -z "${DEBUG}" ] || set -x  # trace if $DEBUG env. var. is non-zero
-SDK_BIN="`dirname "$0" | sed -e "s#^\\([^/]\\)#${PWD}/\\1#"`" # sed makes absolute
-SDK_LIB="$SDK_BIN/../lib"
+[[ -z "${DEBUG}" ]] || set -x  # trace if $DEBUG env. var. is non-zero
 
-JAR_FILE1="$SDK_LIB/opt/tools/appengine-local-endpoints/v1/appengine-local-endpoints.jar"
-if [ ! -e "$JAR_FILE1" ]; then
-  echo "$JAR_FILE1 not found"
+# Construct the absolute name of the SDK bin directory.
+# Use -P so pwd will see the real name, independent of symbolic links.
+readonly SDK_BIN="$(cd -P "$(dirname "$0")" && pwd)"
+readonly SDK_LIB="$(dirname "${SDK_BIN}")/lib"
+
+readonly JAR_FILE1="${SDK_LIB}/opt/tools/appengine-local-endpoints/v1/\
+appengine-local-endpoints.jar"
+if [[ ! -e "${JAR_FILE1}" ]]; then
+  echo "${JAR_FILE1} not found"
   exit 1
 fi
 
-JAR_FILE2="$SDK_LIB/opt/user/appengine-endpoints/v1/appengine-endpoints.jar"
-if [ ! -e "$JAR_FILE2" ]; then
-  echo "$JAR_FILE2 not found"
+readonly JAR_FILE2="${SDK_LIB}/opt/user/appengine-endpoints/v1/\
+appengine-endpoints.jar"
+if [[ ! -e "${JAR_FILE2}" ]]; then
+  echo "${JAR_FILE2} not found"
   exit 1
 fi
 
-CLASSPATH="$JAR_FILE1:$JAR_FILE2:$SDK_LIB/shared/servlet-api.jar:$SDK_LIB/appengine-tools-api.jar:$SDK_LIB/opt/user/datanucleus/v1/jdo2-api-2.3-eb.jar"
-for jar in "$SDK_LIB"/user/*.jar; do
-  CLASSPATH="$CLASSPATH:$jar"
+CLASSPATH="${JAR_FILE1}:${JAR_FILE2}:${SDK_BIN}/jetty94/jetty-home/lib/servlet-api-3.1.jar:\
+${SDK_LIB}/appengine-tools-api.jar:\
+${SDK_LIB}/opt/user/datanucleus/v1/jdo2-api-2.3-eb.jar"
+for jar in "${SDK_LIB}"/user/*.jar; do
+  CLASSPATH="${CLASSPATH}:$jar"
 done
 
-java -cp "$CLASSPATH" com.google.api.server.spi.tools.EndpointsTool "$@"
+readonly SCRIPT_NAME=$(basename "$0")
+readonly RUN_JAVA=$(dirname "$0")/run_java.sh
+exec "${RUN_JAVA}" "${SCRIPT_NAME}" \
+    -cp "${CLASSPATH}" com.google.api.server.spi.tools.EndpointsTool "$@"
